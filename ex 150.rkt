32a1b5a5a20d17c5580d688a51b0b8731e62fe1a
Andrew Tran <atran246@gmail.com>
2013-02-21 12:23:23 -0600

    Ex 150 Data Definitions

---
+++ ex 150.rkt
@@ -0,0 +1,19 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |ex 150|) (read-case-sensitive #t) (teachpacks ((lib "batch-io.ss" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "batch-io.ss" "teachpack" "2htdp")))))
+; List-of-strings is one of:
+; empty
+; (cons String List-of-strings)
+
+; A LLS is one of: 
+; – empty
+; – (cons Los LLS)
+; interp. a list of lines, each line is a list of strings
+
+
+
+
+ 
+
+
+
